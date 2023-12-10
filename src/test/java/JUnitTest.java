0e4d3cf723683fb386594b3b2036f55383e93f11
Chanmi An <bangoul345@gmail.com>
2023-12-10 14:20:32 +0900

    test 코드 작성

---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+    @DisplayName("1 + 2는 3이다") //테스트 이름 명시
+    @Test //테스트를 수행하는 메소드
+    public void junitTest(){
+        int a = 1;
+        int b = 2;
+        int sum = 3;
+
+        Assertions.assertEquals(sum, a + b);
+    }
+
+    @DisplayName("1 + 3은 4이다")
+    @Test
+    public void junitFailedTest(){
+        int a = 1;
+        int b = 3;
+        int sum = 3;
+
+        Assertions.assertEquals(sum, a + b);
+    }
+}
